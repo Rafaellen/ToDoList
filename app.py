c1bb3529629e85b7928c7757852bed130f13ecb9
Rafaellen <bandeirarafaellen@gmail.com>
2024-09-12 23:40:43 -0300

    final version

--- app.py
+++ app.py
@@ -1,10 +1,62 @@
-from flask import Flask, render_template, redirect, request, url_for
-from models import NewTask, StatusEnum
+from flask import Flask, render_template, redirect, request, url_for, flash
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from models import NewTask, StatusEnum, User
 from db import db
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretKey'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(id_usuario):
+    return User.query.get(int(id_usuario))
+
+
+@app.route('/registrar', methods=['GET', 'POST'])
+def registrar():
+    if request.method == 'POST':
+        NomeUsuario = request.form['nome_usuario']
+        SenhaUsuario = request.form['senha_usuario']
+
+        if User.query.filter_by(nome_usuario=NomeUsuario).first():
+            flash('Nome de usuário já existente!')
+            return redirect(url_for('registrar'))
+
+        usuario = User(nome_usuario=NomeUsuario)
+        usuario.definir_senha(SenhaUsuario)
+        db.session.add(usuario)
+        db.session.commit()
+        login_user(usuario)
+        return redirect(url_for('all_tarefas'))
+    return render_template('registroUsuario.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        NomeUsuario = request.form['nome_usuario']
+        SenhaUsuario = request.form['senha_usuario']
+        usuario = User.query.filter_by(nome_usuario=NomeUsuario).first()
+        if usuario and usuario.checar_senha(SenhaUsuario):
+            login_user(usuario)
+            return redirect(url_for('all_tarefas'))
+        else:
+            flash('Usuário e/ou senha incorreto!', 'danger')
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 
 @app.route('/')
@@ -13,31 +65,36 @@ def home():
 
 
 @app.route('/alltarefas')
+@login_required
 def all_tarefas():
-    # Obtém o filtro de status da URL
     status_filter = request.args.get('status')
 
+    query = NewTask.query.filter_by(user_id=current_user.id)
+
     if status_filter and status_filter in StatusEnum.__members__:
-        tarefas = NewTask.query.filter_by(
-            status=StatusEnum[status_filter]).all()
-    else:
-        tarefas = NewTask.query.all()
+        query = query.filter_by(status=StatusEnum[status_filter])
+
+    tarefas = query.all()
 
     return render_template('listar_tarefas.html', tarefas=tarefas, status_filter=status_filter)
 
 
 @app.route('/novatarefa', methods=['GET', 'POST'])
+@login_required
 def nova_tarefa():
     if request.method == 'GET':
         return render_template('registrar_tarefa.html')
     elif request.method == 'POST':
-        id = request.form['idForm']
         titulo = request.form['tituloForm']
         descricao = request.form['descricaoForm']
         status = request.form['statusForm']
 
-        nova_tarefa = NewTask(id=id, titulo=titulo,
-                              descricao=descricao, status=status)
+        nova_tarefa = NewTask(
+            titulo=titulo,
+            descricao=descricao,
+            status=status,
+            user_id=current_user.id
+        )
 
         db.session.add(nova_tarefa)
         db.session.commit()
@@ -46,8 +103,9 @@ def nova_tarefa():
 
 
 @app.route('/editartarefa/<int:id>', methods=['GET', 'POST'])
+@login_required
 def editar_tarefa(id):
-    tarefa = db.session.query(NewTask).filter_by(id=id).first()
+    tarefa = NewTask.query.filter_by(id=id, user_id=current_user.id).first()
     if request.method == 'GET':
         if tarefa:
             return render_template('editar_tarefa.html', tarefa=tarefa)
@@ -74,8 +132,9 @@ def editar_tarefa(id):
 
 
 @app.route('/removertarefa/<int:id>', methods=['POST'])
+@login_required
 def remover_tarefa(id):
-    tarefa = db.session.query(NewTask).filter_by(id=id).first()
+    tarefa = NewTask.query.filter_by(id=id, user_id=current_user.id).first()
 
     if tarefa:
         return redirect(url_for('confirmar_remocao', id=id))
@@ -84,8 +143,9 @@ def remover_tarefa(id):
 
 
 @app.route('/confirmarremocao/<int:id>', methods=['GET'])
+@login_required
 def confirmar_remocao(id):
-    tarefa = db.session.query(NewTask).filter_by(id=id).first()
+    tarefa = NewTask.query.filter_by(id=id, user_id=current_user.id).first()
 
     if tarefa:
         return render_template('remover_tarefa.html', tarefa=tarefa)
@@ -94,14 +154,22 @@ def confirmar_remocao(id):
 
 
 @app.route('/excluirtarefa/<int:id>', methods=['POST'])
+@login_required
 def excluir_tarefa(id):
-    tarefa = db.session.query(NewTask).filter_by(id=id).first()
+    tarefa = NewTask.query.filter_by(id=id, user_id=current_user.id).first()
     if tarefa:
         db.session.delete(tarefa)
         db.session.commit()
         return redirect(url_for('all_tarefas'))
     else:
         return render_template('erro.html', mensagem='Tarefa não encontrada'), 404
+
+
+@app.route('/dashboard')
+def dashboard():
+    # Consultar todas as tarefas
+    tarefas = NewTask.query.all()
+    return render_template('dashboard.html', tarefas=tarefas)
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,6 +1,8 @@
+from flask_login import UserMixin
 from sqlalchemy import Enum
 import enum
 from db import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class StatusEnum(enum.Enum):
@@ -15,6 +17,23 @@ class NewTask(db.Model):
     titulo = db.Column(db.String(200), nullable=False)
     descricao = db.Column(db.String(255), nullable=False)
     status = db.Column(Enum(StatusEnum), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'newuser.id'), nullable=False)
+    responsavel = db.relationship(
+        'User', backref=db.backref('tasks', lazy=True))
 
     def __repr__(self):
         return f"<NewTask id={self.id} titulo='{self.titulo}' status='{self.status}'>"
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'newuser'
+    id = db.Column(db.Integer, primary_key=True)
+    nome_usuario = db.Column(db.String(50), unique=True, nullable=False)
+    senha_usuario = db.Column(db.String(60), nullable=False)
+
+    def definir_senha(self, senha_usuario):
+        self.senha_usuario = generate_password_hash(senha_usuario)
+
+    def checar_senha(self, senha_usuario):
+        return check_password_hash(self.senha_usuario, senha_usuario)
